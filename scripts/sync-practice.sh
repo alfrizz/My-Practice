6dbb668690e5fd2e7ceeb18dc4b0c804160dc91d
Alfonso Rizzica <alfrizz@hotmail.it>
2025-08-07 18:41:41 +0200

    sync unison fix

--- scripts/sync-practice.sh
+++ scripts/sync-practice.sh
@@ -8,18 +8,13 @@ export UNISON_NUMBACKUPS=1
 mkdir -p "$UNISON_TMPDIR"
 
 LOG="$HOME/sync-practice-debug.log"
-ROOT1="/home/alfrizz/my_practice"
-ROOT2="/mnt/g/My Drive/Ingegneria/Data Science GD/My-Practice"
 
 echo "=== [$(date '+%F %T')] Starting Unison sync ===" >> "$LOG"
 
-exec /usr/local/bin/unison \
-  -root      "$ROOT1" \
-  -root      "$ROOT2" \
+exec unison \
   -batch \
   -auto \
   -times \
-  -perms     0 \
   -maxbackups 1 \
   -confirmmerge=false \
   -prefer    newer \
@@ -29,15 +24,4 @@ exec /usr/local/bin/unison \
   -watch \
   -debug     all \
   -logfile   "$LOG" \
-  -ignore    "Name .Trash-0" \
-  -ignore    "Name .unison*" \
-  -ignore    "Path **/.unison*/**" \
-  -ignore    "Name .git" \
-  -ignore    "Path .git/**" \
-  -ignore    "Path **/.git/**" \
-  -ignore    "Name __pycache__" \
-  -ignore    "Path **/__pycache__/**" \
-  -ignore    "Name .ipynb_checkpoints" \
-  -ignore    "Path **/.ipynb_checkpoints/**" \
-  -ignore    "Path **/*.log" \
-  -ignore    "Regex .*[Cc]heckpoint.*"
+  my_practice
